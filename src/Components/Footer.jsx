f84d6ccdfeee0054d24b37065b4051786d8cbe58
AllSparkTech100 <ikangenyinabasiono@gmail.com>
2024-12-24 09:03:51 +0100

    up

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,11 +3,11 @@ import { PiLinktreeLogoBold } from "react-icons/pi";
 
 function Footer() {
   return (
-    <div className="h-42 p-8 flex flex-col md:flex-row lg:flex-row mt-60 bg-black justify-between items-center">
+    <div className="h-42 p-10 md:p-8 lg:p-8 flex flex-col md:flex-row lg:flex-row mt-60 bg-slate-950 justify-between items-center">
       <div className="flex-1/2">
         <input type="text" />
       </div>
-      <div className="flex-1/2">
+      <div className="flex-1/2 place-items-center mt-10 md:place-items-start lg:place-items-start">
         <h3 className="text-2xl capitalize text-white">
           connect with us on social media
         </h3>
