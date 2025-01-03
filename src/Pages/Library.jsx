d4eab955c8763059cb0cea7492cdd567b148b7f3
AllSparkTech100 <ikangenyinabasiono@gmail.com>
2025-01-04 00:16:48 +0100

    new

--- src/Pages/Library.jsx
+++ src/Pages/Library.jsx
@@ -3,9 +3,9 @@ import Resources from "../Components/Resources"
 function Library() {
   return (
     <>
-      <div className="relative h-screen">
+      <div className="relative h-96 lg:h-screen">
         <div className="absolute bg-no-repeat inset-0 bg-[url('/seren.jpeg')] bg-cover bg-bottom before:absolute before:inset-0 before:bg-gradient-to-b before:from-black/50 before:to-black/55">
-          <div className="relative p-5 mx-auto my-40 lg:my-64 ">
+          <div className="relative p-5 mx-auto my-20 md:my-15 lg:my-40  ">
             <div className="flex-col justify-center items-center flex gap-8 place-items-center text-white">
               <h4 className=" uppercase text-center text-4xl lg:text-7xl font-bold">
                 our resources
