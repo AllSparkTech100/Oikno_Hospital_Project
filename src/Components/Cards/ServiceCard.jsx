1152d14e104b89d78d10795488ba6fd3eb505b81
AllSparkTech100 <ikangenyinabasiono@gmail.com>
2025-01-09 09:15:06 +0100

    new

--- src/Components/Cards/ServiceCard.jsx
+++ src/Components/Cards/ServiceCard.jsx
@@ -1,11 +1,18 @@
 function ServiceCard() {
     return (
         <>
-            <div className="container mx-auto p-4 h-96 bg-red-500">
-                <div className="grid grid-cols-1 lg:grid-cols-3">
 
-</div>
-</div>
+            <div className="grid grid-cols-1 gap-4 p-4 lg:grid-cols-3">
+                <div>
+                    <h4 className="capitalize">suicidal ideation</h4>
+                    <p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti minus dicta at modi provident molestias qui neque exercitationem consectetur maxime atque veniam, laudantium reprehenderit explicabo. Ipsa ea eligendi quae ducimus?</p>
+                </div>
+                <div>
+                    <h4 className="capitalize">suicidal ideation</h4>
+                    <p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti minus dicta at modi provident molestias qui neque exercitationem consectetur maxime atque veniam, laudantium reprehenderit explicabo. Ipsa ea eligendi quae ducimus?</p>
+                </div>
+            </div>
+
         </>
     )
 }
