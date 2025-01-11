d2fb1929e2c82a143abcd2ecc18fc0c85c0a37ab
AllSparkTech100 <ikangenyinabasiono@gmail.com>
2025-01-11 19:27:17 +0100

    new

--- src/Components/Cards/ContactCard.jsx
+++ src/Components/Cards/ContactCard.jsx
@@ -1,6 +1,6 @@
 import { FaLocationDot } from "react-icons/fa6";
 import { GoMail } from "react-icons/go";
-import { LuSend } from "react-icons/lu";
+import FormData from '../../Components/FormData'
 import { IoCall } from "react-icons/io5";
 import { work } from "../../lib/Images";
 
@@ -16,7 +16,7 @@ function ContactCard() {
               <h3 className="font-bold text-yellow-500 text-xl lg:text-2xl mb-1 uppercase">
                 send us a message
               </h3>
-             
+
               <p className="mt-2 text-lg text-justify">
                 Schedule a session with our highly qualified licensed practitioners. The consultation can be conducted from wherever you feel most comfortable. <span className="font-bold text-2xl uppercase text-black">
                   no travel, no clinics
@@ -24,33 +24,7 @@ function ContactCard() {
               </p>
             </div>
             <div className="mt-8">
-            <form action="">
-              {/* FullName */}
-              <div className="mb-3 flex gap-3 flex-col mt-3">
-                <label className="font-semibold" htmlFor="fullname">Full Name</label>
-                <input type="text" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="fullname" id="" placeholder="e.g James Richard" required />
-
-              </div>
-              {/* Email Address */}
-              <div className="mb-3 cols-span-1 flex gap-3 flex-col mt-3">
-                <label className="font-semibold" htmlFor="email">Email </label>
-                <input type="email" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="fullname" id="" placeholder="e.g jamesrichard@hotmail.com" required />
-              </div>
-              {/* Mobile Number */}
-              <div className="mb-3 flex gap-3 flex-col mt-3">
-                <label className="font-semibold" htmlFor="number">Mobile Number </label>
-                <input type="phone" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="fullname" id="" placeholder="e.g +234123456788" required />
-              </div>
-              {/* Textarea for message */}
-              <div className="mb-3 flex gap-3 flex-col mt-3">
-                <label className="font-semibold" htmlFor="message">Message </label>
-                <textarea className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="" id="" cols="30" rows="10"></textarea>
-              </div>
-
-              <button className="w-full flex items-center justify-center lg:mt-4 mt-7 rounded-md p-2 bg-slate-800 text-white text-center cursor:pointer">
-                <span className="text-white mr-2"><LuSend size={25} /></span> Submit
-              </button>
-            </form>
+              <FormData />
             </div>
           </div>
 
---
+++ src/Components/FormData.jsx
@@ -0,0 +1,37 @@
+import { LuSend } from "react-icons/lu";
+
+function FormData() {
+    return (
+        <>
+            <form action="">
+                {/* FullName */}
+                <div className="mb-3 flex gap-3 flex-col mt-3">
+                    <label className="font-semibold" htmlFor="fullname">Full Name</label>
+                    <input type="text" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="fullname" id="" placeholder="e.g James Richard" required />
+
+                </div>
+                {/* Email Address */}
+                <div className="mb-3 cols-span-1 flex gap-3 flex-col mt-3">
+                    <label className="font-semibold" htmlFor="email">Email </label>
+                    <input type="email" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="fullname" id="" placeholder="e.g jamesrichard@hotmail.com" required />
+                </div>
+                {/* Mobile Number */}
+                <div className="mb-3 flex gap-3 flex-col mt-3">
+                    <label className="font-semibold" htmlFor="number">Mobile Number </label>
+                    <input type="phone" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="fullname" id="" placeholder="e.g +234123456788" required />
+                </div>
+                {/* Textarea for message */}
+                <div className="mb-3 flex gap-3 flex-col mt-3">
+                    <label className="font-semibold" htmlFor="message">Message </label>
+                    <textarea className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="" id="" cols="30" rows="10"></textarea>
+                </div>
+
+                <button className="w-full flex items-center justify-center lg:mt-4 mt-7 rounded-md p-2 bg-slate-800 text-white text-center cursor:pointer">
+                    <span className="text-white mr-2"><LuSend size={25} /></span> Submit
+                </button>
+            </form>
+        </>
+    )
+}
+
+export default FormData
