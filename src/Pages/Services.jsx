fbde252e9fc8698202c9edd6c643f6ed876df802
AllSparkTech100 <ikangenyinabasiono@gmail.com>
2025-01-11 19:15:02 +0100

    new

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,25 +1,8 @@
 import ServiceCard from "../Components/Cards/ServiceCard";
-import * as React from 'react';
-import Box from '@mui/material/Box';
-// import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
 import { LuSend } from "react-icons/lu";
 
 function Services() {
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 700,
-    bgcolor: 'background.paper',
-    border: 'none',
-    boxShadow: 24,
-    p: 4,
-  };
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+
   return (
     <>
       <div className="relative h-80 md:h-[35rem] lg:h-96">
@@ -34,47 +17,7 @@ function Services() {
           </div>
         </div>
       </div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-         <form action="">
-              {/* FullName */}
-              <div className="mb-3 flex gap-3 flex-col mt-3">
-                <label className="font-semibold" htmlFor="fullname">Full Name</label>
-                <input type="text" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="fullname" id="" placeholder="e.g James Richard" required />
 
-              </div>
-              {/* Email Address */}
-              <div className="mb-3 cols-span-1 flex gap-3 flex-col mt-3">
-                <label className="font-semibold" htmlFor="email">Email </label>
-                <input type="email" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="fullname" id="" placeholder="e.g jamesrichard@hotmail.com" required />
-              </div>
-              {/* Mobile Number */}
-              <div className="mb-3 flex gap-3 flex-col mt-3">
-                <label className="font-semibold" htmlFor="number">Mobile Number </label>
-                <input type="phone" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="fullname" id="" placeholder="e.g +234123456788" required />
-              </div>
-              <div>
-                <option value="">
-                  <select name="" id=""></select>
-                </option>
-              </div>
-              {/* Textarea for message */}
-              <div className="mb-3 flex gap-3 flex-col mt-3">
-                <label className="font-semibold" htmlFor="message">Message </label>
-                <textarea className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="" id="" cols="30" rows="10"></textarea>
-              </div>
-
-              <button className="w-full flex items-center justify-center lg:mt-4 mt-7 rounded-md p-2 bg-slate-800 text-white text-center cursor:pointer">
-                <span className="text-white mr-2"><LuSend size={25} /></span> Submit
-              </button>
-            </form>
-        </Box>
-      </Modal>
       <div className="container mt-20 mx-auto p-4 h-auto">
         <h3 className="uppercase underline font-bold text-lg lg:text-2xl">our services / intervention</h3>
         <div className="my-10">
@@ -82,8 +25,8 @@ function Services() {
         </div>
 
         <div className="flex items-center justify-center">
-          <button type="submit" className="text-center p-4 bg-[#02514b] w-60 rounded-lg text-white lg:hover:scale-90 lg:transition-all " onClick={handleOpen}>
-          Book now
+          <button type="submit" className="text-center p-4 bg-[#02514b] w-60 flex items-center justify-center gap-4 rounded-lg text-white lg:hover:scale-90 lg:transition-all ">
+          <a href="/contact" className='flex items-center justify-center gap-4'>  <LuSend size={25}/>Book now</a>
           </button>
         </div>
       </div>
