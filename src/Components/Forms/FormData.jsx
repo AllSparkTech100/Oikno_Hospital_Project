467ffa2fd634ffd2df7d2389868244284639687d
AllSparkTech100 <ikangenyinabasiono@gmail.com>
2025-01-26 00:48:09 +0100

    new

--- src/Components/Forms/FormData.jsx
+++ src/Components/Forms/FormData.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 function FormData() {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
-    const [phone, setPhone] = useState('')
+    const [number, setNumber] = useState('')
     const [message, setMessage] = useState('')
 
     return (
@@ -21,25 +21,25 @@ function FormData() {
                 {/* Email Address */}
                 <div className="mb-3 cols-span-1 flex gap-3 flex-col mt-3">
                     <label className="font-semibold" htmlFor="email">Email </label>
-                    <input type="email" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="email" id="" placeholder="e.g jamesrichard@hotmail.com" required />
+                    <input type="email" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="email" value={email} onChange={(e) => setEmail(e.target.value)} id="" placeholder="e.g jamesrichard@hotmail.com" required />
                 </div>
                 {/* Mobile Number */}
                 <div className="mb-3 flex gap-3 flex-col mt-3">
                     <label className="font-semibold" htmlFor="number">Mobile Number </label>
-                    <input type="phone" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="number" id="" placeholder="e.g +234123456788" required />
+                    <input type="phone" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="number" value={number} onChange={(e) => setNumber(e.target.value)} id="" placeholder="e.g +234123456788" required />
                 </div>
                 {/* Category */}
 
                 {/* Textarea for message */}
                 <div className="mb-3 flex gap-3 flex-col mt-3">
                     <label className="font-semibold" htmlFor="message">Message </label>
-                    <textarea className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="message" id="" cols="30" rows="10" ></textarea>
+                    <textarea className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="message" value={message} onChange={(e) => setMessage(e.target.value)} id="" cols="30" rows="10" ></textarea>
                 </div>
 
-                <button className="w-full flex items-center justify-center lg:mt-4 mt-7 rounded-md p-2 bg-slate-800 text-white text-center cursor:pointer" disabled={name || email || phone || message}>
+                <button className="w-full flex items-center justify-center lg:mt-4 mt-7 rounded-md p-2 bg-slate-800 text-white text-center cursor:pointer" >
                     <span className="text-white mr-2"><LuSend size={25} /></span> Submit
-                </button>
-            </form>
+        </button >
+            </form >
         </>
     );
 }
